413876301f6398758c86b74575297ed90cd595d1
Bagaskara Pramudita <bagaskarapramudita@gmail.com>
2016-11-29 13:00:40 +0700

    setup server boilerplate

---
+++ src/components/SinglePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class SinglePage extends React.Component {
+	render(){
+		return (
+			<h1> single page </h1>
+		)		
+	}
+}
---
+++ src/data/characters.js
@@ -0,0 +1,284 @@
+const characters = 
+	{
+  "error": "OK",
+  "limit": 20,
+  "offset": 0,
+  "number_of_page_results": 20,
+  "number_of_total_results": 149,
+  "status_code": 1,
+  "results": [
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-55090/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/806902-x1.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/806902-x1.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/806902-x1.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/806902-x1.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/806902-x1.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/806902-x1.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/806902-x1.jpg"
+      },
+      "name": "Colonel Bruce Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-59473/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/683069-the_walking_dead___22___23.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/683069-the_walking_dead___22___23.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/683069-the_walking_dead___22___23.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/683069-the_walking_dead___22___23.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/683069-the_walking_dead___22___23.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/683069-the_walking_dead___22___23.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/683069-the_walking_dead___22___23.jpg"
+      },
+      "name": "Bruce Cooper",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-77809/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/2215777-alan_wayne.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/2215777-alan_wayne.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/2215777-alan_wayne.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/2215777-alan_wayne.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/2215777-alan_wayne.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/2215777-alan_wayne.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/2215777-alan_wayne.jpg"
+      },
+      "name": "Alan Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-75677/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1641091-bringthethunder2_cover_full.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1641091-bringthethunder2_cover_full.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1641091-bringthethunder2_cover_full.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1641091-bringthethunder2_cover_full.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1641091-bringthethunder2_cover_full.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1641091-bringthethunder2_cover_full.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1641091-bringthethunder2_cover_full.jpg"
+      },
+      "name": "Wayne Russell",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-3764/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/2103429-brucegordon.jpeg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/2103429-brucegordon.jpeg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/2103429-brucegordon.jpeg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/2103429-brucegordon.jpeg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/2103429-brucegordon.jpeg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/2103429-brucegordon.jpeg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/2103429-brucegordon.jpeg"
+      },
+      "name": "Bruce Gordon",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-77680/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1800843-solomonwayne.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1800843-solomonwayne.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1800843-solomonwayne.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1800843-solomonwayne.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1800843-solomonwayne.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1800843-solomonwayne.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1800843-solomonwayne.jpg"
+      },
+      "name": "Solomon Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-3602/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/3369544-earth2_cv17_f8kuicoufo_.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/3369544-earth2_cv17_f8kuicoufo_.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/3369544-earth2_cv17_f8kuicoufo_.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/3369544-earth2_cv17_f8kuicoufo_.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/3369544-earth2_cv17_f8kuicoufo_.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/3369544-earth2_cv17_f8kuicoufo_.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/3369544-earth2_cv17_f8kuicoufo_.jpg"
+      },
+      "name": "Thomas Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-64903/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1349741-waynegodofthunder.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1349741-waynegodofthunder.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1349741-waynegodofthunder.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1349741-waynegodofthunder.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1349741-waynegodofthunder.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1349741-waynegodofthunder.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1349741-waynegodofthunder.jpg"
+      },
+      "name": "Wayne Static",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-3603/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/3830835-3692808-screen+shot+2014-03-12+at+8.41.17+am%20%282%29.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/3830835-3692808-screen+shot+2014-03-12+at+8.41.17+am%20%282%29.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/3830835-3692808-screen+shot+2014-03-12+at+8.41.17+am%20%282%29.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/3830835-3692808-screen+shot+2014-03-12+at+8.41.17+am%20%282%29.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/3830835-3692808-screen+shot+2014-03-12+at+8.41.17+am%20%282%29.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/3830835-3692808-screen+shot+2014-03-12+at+8.41.17+am%20%282%29.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/3830835-3692808-screen+shot+2014-03-12+at+8.41.17+am%20%282%29.jpg"
+      },
+      "name": "Martha Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-20169/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1519145-preacher11.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1519145-preacher11.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1519145-preacher11.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1519145-preacher11.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1519145-preacher11.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1519145-preacher11.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1519145-preacher11.jpg"
+      },
+      "name": "John Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-45563/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/4095168-huntress.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/4095168-huntress.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/4095168-huntress.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/4095168-huntress.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/4095168-huntress.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/4095168-huntress.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/4095168-huntress.jpg"
+      },
+      "name": "Huntress (Wayne)",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-116986/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/4466596-wayne.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/4466596-wayne.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/4466596-wayne.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/4466596-wayne.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/4466596-wayne.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/4466596-wayne.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/4466596-wayne.jpg"
+      },
+      "name": "Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-99826/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1-male-good-large.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1-male-good-large.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1-male-good-large.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1-male-good-large.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1-male-good-large.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1-male-good-large.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1-male-good-large.jpg"
+      },
+      "name": "Wayne",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-118805/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1-male-good-large.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1-male-good-large.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1-male-good-large.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1-male-good-large.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1-male-good-large.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1-male-good-large.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1-male-good-large.jpg"
+      },
+      "name": "Bruce",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-107898/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/4113970-bruce.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/4113970-bruce.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/4113970-bruce.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/4113970-bruce.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/4113970-bruce.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/4113970-bruce.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/4113970-bruce.jpg"
+      },
+      "name": "Bruce",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-118933/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1-male-good-large.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1-male-good-large.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1-male-good-large.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1-male-good-large.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1-male-good-large.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1-male-good-large.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1-male-good-large.jpg"
+      },
+      "name": "Bruce",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-21822/",
+      "image": null,
+      "name": "Bruce",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-36314/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/3077629-1000px-bruce-findingnemo3d.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/3077629-1000px-bruce-findingnemo3d.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/3077629-1000px-bruce-findingnemo3d.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/3077629-1000px-bruce-findingnemo3d.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/3077629-1000px-bruce-findingnemo3d.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/3077629-1000px-bruce-findingnemo3d.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/3077629-1000px-bruce-findingnemo3d.jpg"
+      },
+      "name": "Bruce",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-126219/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1-male-good-large.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1-male-good-large.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1-male-good-large.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1-male-good-large.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1-male-good-large.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1-male-good-large.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1-male-good-large.jpg"
+      },
+      "name": "Bruce",
+      "resource_type": "character"
+    },
+    {
+      "api_detail_url": "http://comicvine.gamespot.com/api/character/4005-122363/",
+      "image": {
+        "icon_url": "http://comicvine.gamespot.com/api/image/square_avatar/1-male-good-large.jpg",
+        "medium_url": "http://comicvine.gamespot.com/api/image/scale_medium/1-male-good-large.jpg",
+        "screen_url": "http://comicvine.gamespot.com/api/image/screen_medium/1-male-good-large.jpg",
+        "small_url": "http://comicvine.gamespot.com/api/image/scale_small/1-male-good-large.jpg",
+        "super_url": "http://comicvine.gamespot.com/api/image/scale_large/1-male-good-large.jpg",
+        "thumb_url": "http://comicvine.gamespot.com/api/image/scale_avatar/1-male-good-large.jpg",
+        "tiny_url": "http://comicvine.gamespot.com/api/image/square_mini/1-male-good-large.jpg"
+      },
+      "name": "Bruce",
+      "resource_type": "character"
+    }
+  ],
+  "version": "1.0"
+}
